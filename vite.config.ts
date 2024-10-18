169ba6ee332203b37267bc4b9671992142485105
Yu-Sen <hys18011401591@icloud.com>
2024-10-18 18:25:11 +0800

    feat: 修改构建资源路径

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+/*
+ * @Author: 黄昱森
+ * @LastEditors: 黄昱森
+ * @Description:
+ */
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./",
   plugins: [vue()],
-})
+});
